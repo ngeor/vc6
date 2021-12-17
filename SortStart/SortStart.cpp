3edf27ec1cf700611c45d5351e6011e6ec6574d3
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-12-17 07:23:53 +0100

    feat(SortStart): Ported Pascal code from NSSortStart

--- SortStart/SortStart.cpp
+++ SortStart/SortStart.cpp
@@ -1,5 +1,10 @@
-// SortStart.cpp : Defines the entry point for the application.
-//
+// SortStart
+// Based on:
+// {
+// NGSortStart
+// Version 0.0.1 (16/6/1999)
+// Author: Nick Georgiou
+// }
 
 #include "StdAfx.h"
 
@@ -8,10 +13,42 @@ int APIENTRY WinMain(HINSTANCE hInstance,
                      LPSTR     lpCmdLine,
                      int       nCmdShow)
 {
- 	// TODO: Place code here.
+    LPCTSTR sConfirmSort = _T("Do you want to sort the Start Menu?");
+    LPCTSTR sQuestion = _T("Question");
+    LPCTSTR sError = _T("Error");
+    LPCTSTR sErrRegistryRead   = _T("Couldn't read a system registry key");
+    LPCTSTR sErrRegistryDelete = _T("Couldn't delete a system registry key");
+    LPCTSTR sReboot = _T("SortStart has successfully sorted the Start Menu! For this change to take efffect you must restart your computer. Do you want to restart your computer now?");
 
+    // get the user's OK
+    if (MessageBox(NULL, sConfirmSort, sQuestion, MB_ICONQUESTION | MB_YESNO) == IDYES) {
+        HKEY h;
+        // open the proper registry key
+        bool success = RegOpenKeyEx(HKEY_CURRENT_USER,
+            _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer"),
+            0, KEY_ALL_ACCESS, &h) == ERROR_SUCCESS;
+        // did that work?
+        if (!success) {
+            MessageBox(NULL, sErrRegistryRead, sError, MB_ICONERROR);
+        }
+        else {
+            // delete the subkey MenuOrder and there you go!!!
+            success = RegDeleteKey(h, _T("MenuOrder")) == ERROR_SUCCESS;
+            // God, I hope that worked...
+            if (!success) {
+                MessageBox(NULL, sErrRegistryDelete, sError, MB_ICONERROR);
+                // key probably open so we just close it here
+                RegCloseKey(h);
+            }
+            else {
+                // Great, close the registry key!
+                RegCloseKey(h);
+                // Tell him that he MUST reboot
+                if (MessageBox(NULL, sReboot, sQuestion, MB_ICONQUESTION or MB_YESNO) == IDYES) {
+                    ExitWindowsEx(EWX_REBOOT, 0);
+                }
+            }
+        }
+    }
 	return 0;
 }
-
-
-
--- SortStart/StdAfx.h
+++ SortStart/StdAfx.h
@@ -13,7 +13,7 @@
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <windows.h>
-
+#include <tchar.h>
 
 // TODO: reference additional headers your program requires here
 
